698f0108f492ffce2515eafed3ca3845de887131
moritani <i_moritani@bold.ne.jp>
2026-05-21 15:18:08 +0900

    マッピング　森谷担当画面から原担当画面へ

--- src/main/java/com/example/demo/team13/Team13Controller/Team13General/Team13Cart/Team13PayCompleteController.java
+++ src/main/java/com/example/demo/team13/Team13Controller/Team13General/Team13Cart/Team13PayCompleteController.java
@@ -1,13 +1,14 @@
 package com.example.demo.team13.Team13Controller.Team13General.Team13Cart;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @SessionAttributes("User")
 @Controller
-public class Team13CartController {
+public class Team13PayCompleteController {
 //	private final Team13CartService team13CartService;
 	
 	@ModelAttribute("User")
@@ -15,20 +16,10 @@ public class Team13CartController {
 		return new User();
 	}
 
-	@PostMapping(value="/Team13_CartIndex" ,params="confirm")
-	
-	public String pay_complete(){
-		
-		return "team13/Team13General/Team13_Pay_Complete";
-		
-	}
-	
-	@PostMapping(value="/Team13_CartIndex" ,params="back")
-	
-	public String back_cart(){
+	@GetMapping("/Team13_Pay_Complete")
+	public String index(Model model) {
 		
 		return "team13/Team13General/Team13_Cart";
-		
 	}
 	
 }
