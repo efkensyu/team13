65328132c1ced9c175f6a28a63e822618089eb46
moritani <i_moritani@bold.ne.jp>
2026-05-21 14:36:39 +0900

    エラー解消のため、一時コミット

--- src/main/java/com/example/demo/team13/Team13Controller/Team13General/Team13Cart/CartBuyController.java
+++ src/main/java/com/example/demo/team13/Team13Controller/Team13General/Team13Cart/CartBuyController.java
@@ -1,13 +1,27 @@
 package com.example.demo.team13.Team13Controller.Team13General.Team13Cart;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
 public class CartBuyController {
-	@GetMapping("/Buy")
+	
+	@PostMapping(value="/Team13_Buy" ,params="confirm")
+	
 	public String confirm_buy(){
+		
 		return "team13/Team13General/Team13_Cart_Confirm";
+		
 	}
+	
+	
+	
+	@PostMapping(value="/Team13_Buy" ,params="back")
+		
+		public String back_home(){
+			
+			return "team13/Team13General/Team13_Home";
+			
+		}
 }
