ee222512b872f812181c14ac830cfa42b0bcf079
yhara <y_hara@bold.ne.jp>
2026-05-22 11:05:48 +0900

    領収書作成機能　雛形導入まで

---
+++ src/main/java/com/example/demo/team13/Team13Controller/Team13General/Team13Cart/Team13ReciptController.java
@@ -0,0 +1,13 @@
+package com.example.demo.team13.Team13Controller.Team13General.Team13Cart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Team13ReciptController {
+	@GetMapping("/")
+	public void getIndex() {
+		
+		
+	}
+}
---
+++ src/main/java/com/example/demo/team13/Team13Service/Team13General/Team13Cart/MakeRecipt.java
@@ -0,0 +1,56 @@
+package com.example.demo.team13.Team13Service.Team13General.Team13Cart;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class MakeRecipt {
+	
+	public void makeXSSF(HttpServletResponse response)throws IOException{			//エクセルファイル生成。
+		Workbook workbook_ = new XSSFWorkbook();
+		
+		Sheet sheet_ = workbook_.createSheet("ふーたお");
+		
+		//createRow()に渡すのはインデックス。
+		//1行目なら0、2行目なら1。
+		Row row_ = sheet_.createRow(1);
+		
+		//2行目のB列、つまりB2セルに値がセットされる。
+		Cell cell_ = row_.createCell(CellReference.convertColStringToIndex("B"));
+		cell_.setCellValue("私とお茶しない？");
+		
+		
+		//---------------------------------------------------------------------------------------------
+		//ダウンロード。
+		
+		//ファイル名に2バイト文字を使えるように一工夫。
+		response.addHeader(
+				"Content-Disposition", 
+				"attachment; filename*=UTF-8''" + URLEncoder.encode("胡桃.xlsx", StandardCharsets.UTF_8.name()));
+		
+		ServletOutputStream stream_ = response.getOutputStream();
+		workbook_.write(stream_);
+		stream_.close();
+		
+		workbook_.close();
+		
+		
+		//---------------------------------------------------------------------------------------------
+		//responseをいじっているので、『return "/index";』みたいなことをしても、ページ移動しません。
+		//ページ移動せずダウンロードだけ発生することになるので、この関数の戻り値はvoid。
+		
+		//最小限のサンプルコードなのでtry catchは無しです。
+		
+	}
+			
+}
