2e11b34cf0c14fee7d0a55db40d5991567722ff5
moritani <i_moritani@bold.ne.jp>
2026-05-22 11:08:59 +0900

    大まかにテーブル作成

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,1 +1,21 @@
 INSERT INTO team13_shain_tbl VALUES('1', 'test','test_address','test_email','0000000000000');
+
+INSERT INTO team13_shohin_tbl VALUES('00000', 'test', '000000', '00000');
+
+INSERT INTO team13_order_tbl VALUES('0000000000', '00000', '00000', '2026-05-21 18:00:00');
+
+INSERT INTO team13_admin_tbl VALUES('00000', 'test', 'aaaaa');
+
+INSERT INTO team13_shiire_history_tbl VALUES('00000', '00000', '00000', '00000', '2026-05-21 18:00:00');
+
+INSERT INTO team13_shiire_tbl VALUES('00000', 'test', '00000', 'test_adress', '00000000000');
+
+INSERT INTO team13_cart_shohin_table VALUES('00000', 'test', '00000', '000000', '000000');
+
+INSERT INTO team13_shiharai_tbl VALUES('00', 'test');
+
+INSERT INTO team13_card_info_tbl VALUES('00000', 'test', '00000', '0', '000000');
+
+
+
+
