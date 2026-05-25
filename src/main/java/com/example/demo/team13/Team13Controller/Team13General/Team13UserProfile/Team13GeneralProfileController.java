206535bccc4cc0efc6a97f8baae89cb32d164563
nishimura <e_nishimura@bold.ne.jp>
2026-05-26 08:58:50 +0900

    ユーザプロフィール完成

--- src/main/java/com/example/demo/team13/Team13Controller/Team13General/Team13UserProfile/Team13GeneralProfileController.java
+++ src/main/java/com/example/demo/team13/Team13Controller/Team13General/Team13UserProfile/Team13GeneralProfileController.java
@@ -1,15 +1,24 @@
 package com.example.demo.team13.Team13Controller.Team13General.Team13UserProfile;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.team13.Session.Team13LoginSession;
+
 @Controller
+
 public class Team13GeneralProfileController {
+	@Autowired Team13LoginSession team13LoginSession;
 	
 	@GetMapping("/Team13_General_Profile")
-	public String profile(){
+	public String profile(Model model){
 		
-	 return "team13/Team13General/Team13_General_Profile";
+		
+		model.addAttribute("login",team13LoginSession);
+		
+		return "team13/Team13General/Team13_General_Profile";
 	 
 	}
 
