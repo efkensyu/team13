4a0e02a3e94c07374503ef71a15f14282646c101
moritani <i_moritani@bold.ne.jp>
2026-05-26 08:48:45 +0900

    スキーマから仕入れテーブル消した＆商品一覧から在庫更新へデータ渡すの完了

--- src/main/java/com/example/demo/team13/Team13Controller/Team13Admin/Team13Stock/Team13AdminHomeController.java
+++ src/main/java/com/example/demo/team13/Team13Controller/Team13Admin/Team13Stock/Team13AdminHomeController.java
@@ -1,22 +1,48 @@
 package com.example.demo.team13.Team13Controller.Team13Admin.Team13Stock;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.team13.Team13Entity.Team13Shohin;
+import com.example.demo.team13.Team13Service.Team13Admin.Team13Stock.Team13AdminHomeService;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
+@Data
 public class Team13AdminHomeController {
 	
+	private final Team13AdminHomeService team13AdminHomeService;
+	
 	@GetMapping("/Team13_Admin_Home")
-	public String add(){
+	public String add(Model model){
+		 List<Team13Shohin> shohinData = team13AdminHomeService.findAll();
+		 model.addAttribute("shohinData",shohinData);
 		
 	 return "team13/Team13Admin/Team13_Home";
 	 
 	}
 	
-	@PostMapping("/Team13_Admin_Home")
-	public String send(){
-	
+	@PostMapping(value="/Team13_Update_Stock" ,params="shohin_id")
+	public String send(@RequestParam("shohin_id") String shohin_id, @RequestParam("count") int count,Model model){
+		
+		Team13Shohin cartItem = team13AdminHomeService.findById(shohin_id);
+		
+	    model.addAttribute("cartItem",cartItem);
+	    
+	    System.out.println("商品情報を送る");
+	    
+	    model.addAttribute("count",count);
+		
+		System.out.println("商品個数を送る");
+		
     return "team13/Team13Admin/Team13_Update_Stock";
  
 	}
