ccd3f5bb9d44b012427c3da3039785fce3649d6a
nishimura <e_nishimura@bold.ne.jp>
2026-05-25 17:54:19 +0900

    ログ出力完成

--- src/main/java/com/example/demo/team13/Team13Controller/Team13Auth/Team13Logout/Team13AdminLogoutController.java
+++ src/main/java/com/example/demo/team13/Team13Controller/Team13Auth/Team13Logout/Team13AdminLogoutController.java
@@ -1,12 +1,19 @@
 package com.example.demo.team13.Team13Controller.Team13Auth.Team13Logout;
 
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.team13.Team13Controller.Team13Auth.Team13Login.Team13LoginSession;
+
 @Controller
 public class Team13AdminLogoutController {
 
+	@Autowired Team13LoginSession team13LoginSession;
+	
     @GetMapping("/Team13_Admin_Logout")
     public String logout(){
         return "team13/Team13Admin/Team13_Logout";
@@ -18,7 +25,8 @@ public class Team13AdminLogoutController {
     }
     
     @PostMapping(value="/Team13_Admin_Logout",params="logout")
-    public String sendlogout() {
+    public String sendlogout(HttpSession session) {
+    	session.invalidate(); 
         return "team13/Team13Admin/Team13_Login";
     }
 }
